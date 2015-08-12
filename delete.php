7e0835c6261fce0569235fe1bbe74efaf0edfbb8
mbckhs <matimast@gmail.com>
2015-08-12 19:54:03 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,4 +1,4 @@
-<? 
+<?php 
 include('config.php'); 
 $id = (int) $_GET['id']; 
 mysql_query("DELETE FROM `usuario` WHERE `id` = '$id' ") ; 
@@ -6,3 +6,5 @@ echo (mysql_affected_rows()) ? "Row deleted.<br /> " : "Nothing deleted.<br /> "
 ?> 
 
 <a href='list.php'>Back To Listing</a>
+
+
