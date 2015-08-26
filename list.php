13664a0207f04dffb8b95ef57ac69fa89b269435
mbckhs <matimast@gmail.com>
2015-08-26 16:43:58 -0300

    new

--- list.php
+++ list.php
@@ -1,20 +1,16 @@
-<?php
+<? 
 include('config.php'); 
 echo "<table border=1 >"; 
 echo "<tr>"; 
 echo "<td><b>Id</b></td>"; 
-echo "<td><b>Firstname</b></td>"; 
-echo "<td><b>Lastname</b></td>"; 
-echo "<td><b>Email</b></td>"; 
+echo "<td><b>Username</b></td>"; 
 echo "</tr>"; 
 $result = mysql_query("SELECT * FROM `usuario`") or trigger_error(mysql_error()); 
 while($row = mysql_fetch_array($result)){ 
 foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
 echo "<tr>";  
 echo "<td valign='top'>" . nl2br( $row['id']) . "</td>";  
-echo "<td valign='top'>" . nl2br( $row['firstname']) . "</td>";  
-echo "<td valign='top'>" . nl2br( $row['lastname']) . "</td>";  
-echo "<td valign='top'>" . nl2br( $row['email']) . "</td>";  
+echo "<td valign='top'>" . nl2br( $row['username']) . "</td>";  
 echo "<td valign='top'><a href=edit.php?id={$row['id']}>Edit</a></td><td><a href=delete.php?id={$row['id']}>Delete</a></td> "; 
 echo "</tr>"; 
 } 
