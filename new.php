4fb4259541a4bca663d55b003bc7d9d55f77ba27
mbckhs <matimast@gmail.com>
2015-08-26 17:16:00 -0300

    Update new.php

--- new.php
+++ new.php
@@ -1,8 +1,10 @@
 <?php
 include('config.php'); 
 if (isset($_POST['submitted'])) { 
+  $has=$_POST['password'];
+  $has1=hash('sha256',$has);
 foreach($_POST AS $key => $value) { $_POST[$key] = mysql_real_escape_string($value); } 
-$sql = "INSERT INTO `usuario` ( `username` ,  `password`  ) VALUES(  '{$_POST['username']}' ,  '{$_POST['password']}'  ) "; 
+$sql = "INSERT INTO `usuario` ( `username` ,  `password`  ) VALUES(  '{$_POST['username']}' ,  '{$has1}'  ) "; 
 mysql_query($sql) or die(mysql_error()); 
 echo "Added row.<br />"; 
 echo "<a href='list.php'>Back To Listing</a>"; 
