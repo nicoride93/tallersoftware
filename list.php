939bd293831319c58ea0ab1207994453db665dfb
mbckhs <matimast@gmail.com>
2015-08-26 17:37:56 -0300

    Update list.php

--- list.php
+++ list.php
@@ -18,9 +18,10 @@ echo "</tr>";
 } 
 echo "</table>"; 
 echo "<a href=new.php>New Row</a>"; 
-
+/*
 $sql=mysql_query('call mostrarusuarios')
 while($arr = mysql_fetch_array($sql)) {
   echo $arr[1]
 }
+*/
 ?>
