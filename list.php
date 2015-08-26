f8d7ebd8a644fd13376dbd7de2c7ce9e60957b98
mbckhs <matimast@gmail.com>
2015-08-26 17:09:18 -0300

    Update list.php

--- list.php
+++ list.php
@@ -4,6 +4,7 @@ echo "<table border=1 >";
 echo "<tr>"; 
 echo "<td><b>Id</b></td>"; 
 echo "<td><b>Username</b></td>"; 
+echo "<td><b>Password</b></td>"; 
 echo "</tr>"; 
 $result = mysql_query("SELECT * FROM `usuario`") or trigger_error(mysql_error()); 
 while($row = mysql_fetch_array($result)){ 
@@ -11,6 +12,7 @@ foreach($row AS $key => $value) { $row[$key] = stripslashes($value); }
 echo "<tr>";  
 echo "<td valign='top'>" . nl2br( $row['id']) . "</td>";  
 echo "<td valign='top'>" . nl2br( $row['username']) . "</td>";  
+echo "<td valign='top'>" . nl2br( $row['password']) . "</td>";  
 echo "<td valign='top'><a href=edit.php?id={$row['id']}>Edit</a></td><td><a href=delete.php?id={$row['id']}>Delete</a></td> "; 
 echo "</tr>"; 
 } 
