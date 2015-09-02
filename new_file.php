9833b5a5fe1cc6d294b216a62ef231bde5fac609
mbckhs <matimast@gmail.com>
2015-09-02 15:25:30 -0300

    casi

--- new_file.php
+++ new_file.php
@@ -5,8 +5,7 @@ include('config.php');
 while ($arr = mysql_fetch_assoc($sql)) {
 	
 		$has=hash('sha256',$arr['password']);
-		echo $arr['id']." ".$has."<br/>";
-		//$upd = mysql_query("UPDATE `usuario` SET `password` = '{$has}' where `id`= '{$arr['id']}' ");
+		$upd = "UPDATE `usuario` SET `password` = '{$has}' where `id`= '{$arr['id']}' <br/>";
 	$count++;
 }
 echo $count;
