6646cd984275475dbb797d763d37ffa01377fda1
mbckhs <matimast@gmail.com>
2015-10-21 20:09:19 -0300

    s

--- trans.php
+++ trans.php
@@ -1,22 +1,22 @@
 <?php
 include ('config.php');
 
-echo "<select name='o'>";
+echo "<select name='o'>Cuenta origen:";
 $result = mysql_query("call mostarusuarios") or trigger_error(mysql_error());
 while ($row = mysql_fetch_array($result)) {
 	foreach ($row AS $key => $value) {
 		$row[$key] = stripslashes($value);
 	}
-	echo "Cuenta origen:<option value=" . nl2br($row['nombre']) . ">" . nl2br($row['nombre']) . "</option>";
+	echo "<option value=" . nl2br($row['nombre']) . ">" . nl2br($row['nombre']) . "</option>";
 }
 echo "</select><br/>";
 
-echo "<select name='d'>";
+echo "<select name='d'>Cuenta destino:";
 while ($row1 = mysql_fetch_array($result)) {
 	foreach ($row1 AS $key1 => $value1) {
 		$row1[$key1] = stripslashes($value1);
 	}
-	echo "Cuenta destino:<option value=" . nl2br($row1['nombre']) . ">" . nl2br($row1['nombre']) . "</option>";
+	echo "<option value=" . nl2br($row1['nombre']) . ">" . nl2br($row1['nombre']) . "</option>";
 }
 echo "</select><br/>
 
