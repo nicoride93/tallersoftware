5986c3ff0c1f4b27c3c54b9837d3ee2c6c534776
mbckhs <matimast@gmail.com>
2015-10-21 19:46:32 -0300

    asd

--- trans.php
+++ trans.php
@@ -1,6 +1,17 @@
 
-
+<html>
 <?php
     include ('config.php');
+	?>
+	<select>
+	<?php
+	$result = mysql_query("call mostarusuarios") or trigger_error(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+	foreach ($row AS $key => $value) { $row[$key] = stripslashes($value);
+	}
+	
+	echo "<option value=".$row['nombre'].">" . nl2br($row['nombre']) . "</option>";
 	
 ?>
+</select>
+</html>
