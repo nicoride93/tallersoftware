67ca36538ef2314264069306b45d602bc200f340
mbckhs <matimast@gmail.com>
2015-10-21 20:02:30 -0300

    s

--- trans.php
+++ trans.php
@@ -1,13 +1,30 @@
 <?php
 include ('config.php');
 
-echo "<select>";
+echo "<select name='o'>";
 $result = mysql_query("call mostarusuarios") or trigger_error(mysql_error());
 while ($row = mysql_fetch_array($result)) {
 	foreach ($row AS $key => $value) {
 		$row[$key] = stripslashes($value);
 	}
-	echo "<option value=" . nl2br($row['nombre']) . ">" . nl2br($row['nombre']) . "</option>";
+	echo "Cuenta origen:<option value=" . nl2br($row['nombre']) . ">" . nl2br($row['nombre']) . "</option>";
 }
-echo "</select>";
+echo "</select><br/>";
+
+echo "<select name='d'>";
+$result = mysql_query("call mostarusuarios") or trigger_error(mysql_error());
+while ($row = mysql_fetch_array($result)) {
+	foreach ($row AS $key => $value) {
+		$row[$key] = stripslashes($value);
+	}
+	echo "Cuenta destino:<option value=" . nl2br($row['nombre']) . ">" . nl2br($row['nombre']) . "</option>";
+}
+echo "</select><br/>
+
+
+<form method='post' action='depo.php'>
+	<input type='text' name='monto' value='Ingrese el monto'/>
+	<input type='submit' value='Depositar'/>
+</form>
+";
 ?>
