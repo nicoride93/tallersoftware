f1faece11eaeeb131cb5d76a3b47d54c9ed3dfe1
mbckhs <matimast@gmail.com>
2015-10-21 19:54:49 -0300

    a

--- trans.php
+++ trans.php
@@ -1,17 +1,13 @@
-
-<html>
 <?php
-    include ('config.php');
-	?>
-	<select>
-	<?php
-	$result = mysql_query("call mostarusuarios") or trigger_error(mysql_error());
+include ('config.php');
+
+echo "<select>";
+$result = mysql_query("call mostarusuarios") or trigger_error(mysql_error());
 while ($row = mysql_fetch_array($result)) {
-	foreach ($row AS $key => $value) { $row[$key] = stripslashes($value);
+	foreach ($row AS $key => $value) {
+		$row[$key] = stripslashes($value);
 	}
-	
-	echo "<option value=".$row['nombre'].">" . nl2br($row['nombre']) . "</option>";
-	
+	echo "<option value=" . nl2br($row['nombre']) . ">" . nl2br($row['nombre']) . "</option>";
+}
+echo "</select>";
 ?>
-</select>
-</html>
