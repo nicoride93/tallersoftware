7f1094a97688d26b474a3c83e8f551434d82a462
mbckhs <matimast@gmail.com>
2015-10-21 19:37:13 -0300

    asdasdasdsad

--- list.php
+++ list.php
@@ -11,7 +11,7 @@ echo "<td><b>Username</b></td>";
 echo "<td><b>Password</b></td>";
 echo "
 	</tr>";
-$result = mysql_query("call mostrarusuarios") or trigger_error(mysql_error());
+$result = mysql_query("call mostarusuarios") or trigger_error(mysql_error());
 while ($row = mysql_fetch_array($result)) {
 	foreach ($row AS $key => $value) { $row[$key] = stripslashes($value);
 	}
